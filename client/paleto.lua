816380a0dcebb4a4cc677cb577882a07b82084d9
smitthyy <adamjchapman@pm.me>
2022-10-03 12:57:19 +0100

    Replaced cops function for ps-dispatch

--- client/paleto.lua
+++ client/paleto.lua
@@ -39,7 +39,7 @@ RegisterNetEvent('qb-bankrobbery:UseBankcardA', function()
                         TriggerServerEvent('qb-bankrobbery:server:removeBankCard', '01')
                         Config.DoorlockAction(4, false)
                         if copsCalled or not Config.BigBanks["paleto"]["alarm"] then return end
-                        TriggerServerEvent("qb-bankrobbery:server:callCops", "paleto", 0, pos)
+                        local camId = Config.BigBanks["pacific"]["camId"] exports['ps-dispatch']:PacificBankRobbery(camId)
                         copsCalled = true
                     end, function() -- Cancel
                         StopAnimTask(ped, "anim@gangops@facility@servers@", "hotwire", 1.0)
